a3e0d4e02386b40927db8ed7931e8b995a3eeb35
rscibelli <rscibelli@umass.edu>
2020-01-30 17:19:05 -0500

    Updates

---
+++ precache-manifest.77d4dfa6fb3bfb4bd3ed0152465f22c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f002630ba22670eca73857a96a6966f4",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "2cfca4c7d1e3ff0951dc",
+    "url": "/create-react-app/static/js/2.a5e2c64e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/create-react-app/static/js/2.a5e2c64e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa8b2d1032be7c554c68",
+    "url": "/create-react-app/static/js/main.37aca3b2.chunk.js"
+  },
+  {
+    "revision": "170034edd845cb42b935",
+    "url": "/create-react-app/static/js/runtime-main.85b12498.js"
+  }
+]);
